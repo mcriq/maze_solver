0aba70b1a45936a88fae46317d0998fab0b0ee90
mcriq <125591030+mcriq@users.noreply.github.com>
2025-01-15 17:02:29 -0600

    finish maze solver

--- src/test_maze.py
+++ src/test_maze.py
@@ -1,5 +1,7 @@
 import unittest
+
 from maze import Maze
+
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
@@ -25,8 +27,19 @@ class Tests(unittest.TestCase):
         )
         self.assertEqual(
             m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
-            False
+            False,
         )
+
+    def test_maze_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
 
 
 if __name__ == "__main__":
