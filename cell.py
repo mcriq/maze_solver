6e578b3d11e8fc2ab783b8c7e2cd5117f3b7dea1
mcriq <125591030+mcriq@users.noreply.github.com>
2025-01-15 10:52:37 -0600

    create initial classes for maze solver

---
+++ cell.py
@@ -0,0 +1,49 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+    
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = self._x1 + half_length
+        y_center = self._y1 + half_length
+        point_1 = Point(x_center, y_center)
+
+        half_length = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = to_cell._x1 + half_length
+        y_center2 = to_cell._y1 + half_length
+        point_2 = Point(x_center2, y_center2)
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        line = Line(point_1, point_2)
+        self._win.draw_line(line, fill_color)
